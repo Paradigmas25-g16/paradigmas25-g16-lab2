4c8c23c99a12784c673e21a3a7bb19cf08c2169d
Juakinsote <joavazquez@mi.unc.edu.ar>
2025-05-22 22:03:20 -0300

    implementado el main del proyecto y el archivo que asigna tema a las entidades

--- src/FeedReaderMain.java
+++ src/FeedReaderMain.java
@@ -1,126 +1,121 @@
+import java.util.*;
+
+import feed.*;
+import parser.*;
+import subscription.*;
+import httpRequest.*;
+import namedEntity.*;
+import namedEntity.heuristic.*;
+
 public class FeedReaderMain {
+
+	private static String parseSubUrl(String filePath){
+		Scanner stdin = new Scanner(System.in);
+		String URLCompleted = null;
+
+        SubscriptionParser parser = new SubscriptionParser();
+        Subscription subs = parser.parse(filePath);
+
+        List<SingleSubscription> subsList = subs.getSubscriptionsList();
+        System.out.println("Choose a URL (1-"+ subsList.size() +"):");    
+        System.out.println("Options:\n");
+        for(int i=0 ; i < subsList.size() ; i++){
+            int index = i+1;
+            System.out.println("("+ index +") "+subsList.get(i).getUrl() + "\n");              //prints all the url types from subs
+        }
+        System.out.print("\nOption: ");
+        int inputUrl = stdin.nextInt();                                   //gets from stdin the url type
+        if (inputUrl>subsList.size() || inputUrl<1){
+            System.err.println("Option out of range");
+        }else{
+            SingleSubscription singlesub = subs.getSingleSubscription(inputUrl-1);
+
+            System.out.println("Choose a parameter for '%s'(1-"+ singlesub.getUlrParamsSize() +"):");
+            System.out.println("Options:\n");
+            for(int i=0 ; i < singlesub.getUlrParamsSize() ; i++){
+                int index = i+1;
+                System.out.println("("+ index +") "+singlesub.getUlrParams(i) + "\n");
+            }
+            System.out.print("\nOption: ");
+            int inputParam = stdin.nextInt();
+            if(inputParam>singlesub.getUlrParamsSize() || inputParam<1){
+                System.err.println("Option out of range");
+            }else{
+                URLCompleted = singlesub.getFeedToRequest(inputParam-1);
+
+            }
+        }
+		stdin.close();
+		return URLCompleted;
+	}
+
+	private static String feedRequest(String URL){
+		httpRequester requester = new httpRequester();
+		String feedData = requester.getFeedContent(URL);
+
+		return feedData;
+	}
+	
+	private static Feed parseFeed(String URL, String feedData){
+		Feed feed = null;
+		if(URL.contains("rss.nytimes.com")){
+			RssParser parser = new RssParser();
+			feed = parser.parse(feedData);
+		}else if(URL.contains("www.reddit.com")){
+			RedditParser parser = new RedditParser();
+			feed = parser.parse(URL);
+		}
+		return feed;
+	}
+
+	private static List<NamedEntity> computeEntities(Feed feed){
+		QuickHeuristic quickHeuristic = new QuickHeuristic();
+		Application application = new Application();
+		List <NamedEntity> namedEntities = new ArrayList<>();	
+		namedEntities = application.main(feed, quickHeuristic);
+
+		return namedEntities;
+	}
 
 	private static void printHelp(){
 		System.out.println("Please, call this program in correct way: FeedReader [-ne]");
 	}
-		
+	
+	
 	public static void main(String[] args) {
 		System.out.println("************* FeedReader version 1.0 *************");
+		String filepath = "config/subscriptions.json";
+
 		if (args.length == 0) {
-    		// 1. Leer archivo de suscripción por defecto
-    		
-			System.out.println("Leyendo el archivo de suscripcion por defecto");
-			String jsonpath = "config/subscriptions.json";
-    		parser.SubscriptionParser parser = new parser.SubscriptionParser();
-    		subscription.Subscription subs = parser.parse(jsonpath);
 
-    		// 2. Mostrar opciones y pedir input al usuario
-    		java.util.List<subscription.SingleSubscription> subsList = subs.getSubscriptionsList();
-    		System.out.println("Elija una URL (1-" + subsList.size() + "):");
-    		for (int i = 0; i < subsList.size(); i++) {
-    		    System.out.println("(" + (i + 1) + ") " + subsList.get(i).getUrl());
-    		}
-    		System.out.print("Opción: ");
-    		java.util.Scanner stdin = new java.util.Scanner(System.in);
-    		int inputUrl = stdin.nextInt();
-    		if (inputUrl < 1 || inputUrl > subsList.size()) {
-    		    System.err.println("Opción fuera de rango");
-    		    stdin.close();
-    		    return;
-    		}
-    		subscription.SingleSubscription singleSub = subs.getSingleSubscription(inputUrl - 1);
+    		System.out.println("\n-----Parsing subscriptions.json-----\n");
+			String UrlSub = parseSubUrl(filepath);
 
-    		System.out.println("Elija un parámetro para '%s' (1-" + singleSub.getUlrParamsSize() + "):");
-    		for (int i = 0; i < singleSub.getUlrParamsSize(); i++) {
-    		    System.out.println("(" + (i + 1) + ") " + singleSub.getUlrParams(i));
-    		}
-    		System.out.print("Opción: ");
-    		int inputParam = stdin.nextInt();
-    		if (inputParam < 1 || inputParam > singleSub.getUlrParamsSize()) {
-    		    System.err.println("Opción fuera de rango");
-    		    stdin.close();
-    		    return;
-    		}
-    		String urlCompleted = singleSub.getFeedToRequest(inputParam - 1);
-    		stdin.close();
+			System.out.println("\n-----Requesting feed with HTTP request-----\n");
+			String feedData = feedRequest(UrlSub);
 
-    		// 3. Obtener el feed del servidor
-    		httpRequest.httpRequester requester = new httpRequest.httpRequester();
-    		String feedData = requester.getFeedContent(urlCompleted);
+			System.out.println("\n-----Parsing feed-----\n");
+			Feed feed = parseFeed(UrlSub, feedData);
 
-    		// 4. Parsear el feed
-    		feed.Feed feed = null;
-    		if (urlCompleted.contains("rss.nytimes.com")) {
-    		    parser.RssParser rssParser = new parser.RssParser();
-    		    feed = rssParser.parse(feedData);
-    		} else if (urlCompleted.contains("www.reddit.com")) {
-    		    parser.RedditParser redditParser = new parser.RedditParser();
-    		    feed = redditParser.parse(urlCompleted);
-    		}
-
-    		// 5. Mostrar los artículos
-    		if (feed != null) {
-    		    feed.prettyPrint();
-    		} else {
-    		    System.err.println("No se pudo obtener el feed.");
-    		}
-
+			feed.prettyPrint();
 		} else if (args.length == 1){
-            // 1. Leer archivo de suscripción por defecto
-			System.out.println("Leyendo el archivo de suscripcion por defecto");
-            String jsonpath = "config/subscriptions.json";
-            parser.SubscriptionParser parser = new parser.SubscriptionParser();
-            subscription.Subscription subs = parser.parse(jsonpath);
+            System.out.println("\n-----Parsing subscriptions.json-----\n");
+			String UrlSub = parseSubUrl(filepath);
 
-            // 2. Mostrar opciones y pedir input al usuario
-    		java.util.List<subscription.SingleSubscription> subsList = subs.getSubscriptionsList();
-    		System.out.println("Elija una URL (1-" + subsList.size() + "):");
-    		for (int i = 0; i < subsList.size(); i++) {
-    		    System.out.println("(" + (i + 1) + ") " + subsList.get(i).getUrl());
-    		}
-    		System.out.print("Opción: ");
-    		java.util.Scanner stdin = new java.util.Scanner(System.in);
-    		int inputUrl = stdin.nextInt();
-    		if (inputUrl < 1 || inputUrl > subsList.size()) {
-    		    System.err.println("Opción fuera de rango");
-    		    stdin.close();
-    		    return;
-    		}
-    		subscription.SingleSubscription singleSub = subs.getSingleSubscription(inputUrl - 1);
+			System.out.println("\n-----Requesting feed with HTTP request-----\n");
+			String feedData = feedRequest(UrlSub);
 
-    		System.out.println("Elija un parámetro para '%s' (1-" + singleSub.getUlrParamsSize() + "):");
-    		for (int i = 0; i < singleSub.getUlrParamsSize(); i++) {
-    		    System.out.println("(" + (i + 1) + ") " + singleSub.getUlrParams(i));
-    		}
-    		System.out.print("Opción: ");
-    		int inputParam = stdin.nextInt();
-    		if (inputParam < 1 || inputParam > singleSub.getUlrParamsSize()) {
-    		    System.err.println("Opción fuera de rango");
-    		    stdin.close();
-    		    return;
-    		}
-    		String urlCompleted = singleSub.getFeedToRequest(inputParam - 1);
-    
+			System.out.println("\n-----Parsing feed-----\n");
+			Feed feed = parseFeed(UrlSub, feedData);
 
-           	// 3. Obtener el feed del servidor
-    		httpRequest.httpRequester requester = new httpRequest.httpRequester();
-    		String feedData = requester.getFeedContent(urlCompleted);
-
-            // 4. Parsear el feed
-            feed.Feed feed = null;
-            if (urlCompleted.contains("rss.nytimes.com")) {
-                parser.RssParser rssParser = new parser.RssParser();
-                feed = rssParser.parse(feedData);
-            } else if (urlCompleted.contains("www.reddit.com")) {
-                parser.RedditParser redditParser = new parser.RedditParser();
-                feed = redditParser.parse(urlCompleted);
-            }
-
-            // 5. Calcular entidades nombradas y mostrar tabla
-			/*
-			 * Hacer la parte de la heuristica para calcular las entidades nombradas
-			 */
-    		stdin.close();
+			System.out.println("\n-----Computing namedEntities-----\n");
+			List<NamedEntity> namedEntities = computeEntities(feed);
+			
+			System.out.println("\n-----prettyPrint namedEntities-----\n");
+			namedEntities.forEach(entity -> System.out.println(entity.toString()));
+			Application application = new Application();
+			application.categoryTable(namedEntities);
+			
         } else {
             printHelp();
         }
